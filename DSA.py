822b7b69d939109f46abbc2cc303ca5be1933808
shine2135 <mankarshahin@gmail.com>
2025-08-05 08:23:01 +0530

    prac

--- DSA.py
+++ DSA.py
@@ -10,3 +10,18 @@ class Solution:
         while l < r:
             s[l], s[r] = s[r], s[l]
             l, r = l + 1, r - 1
+#121. Best Time to Buy and Sell Stock
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        maxP = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxP = max(maxP, profit)
+            else:
+                l = r
+            r += 1
+
+        return maxP
